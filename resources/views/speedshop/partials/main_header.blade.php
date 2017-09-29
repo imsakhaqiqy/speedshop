f3ddd6a69a8cf61ec4ffe6476188ab0d92068467
imsakhaqiqy <imsakhaqiqy24@gmail.com>
2017-09-30 04:14:22 +0700

    add header search

--- resources/views/speedshop/partials/main_header.blade.php
+++ resources/views/speedshop/partials/main_header.blade.php
@@ -687,15 +687,31 @@
     				</div>
 				</li>
 		 </ul>-->
-		 <div class="dropdown">
-		   <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-		   <span class="caret"></span></button>
-		   <ul class="dropdown-menu">
-		     <li><a href="#">HTML</a></li>
-		     <li><a href="#">CSS</a></li>
-		     <li><a href="#">JavaScript</a></li>
-		   </ul>
-		 </div>
+		 	<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+					<ul class="nav navbar-nav" style="width:20%">
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Category
+							<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Page 1-1</a></li>
+								<li><a href="#">Page 1-2</a></li>
+								<li><a href="#">Page 1-3</a></li>
+							</ul>
+						</li>
+					</ul>
+						<form class="navbar-form navbar-left" style="width:80%">
+							<div class="input-group">
+						    <input type="text" class="form-control" placeholder="Search">
+						    <div class="input-group-btn">
+						      <button class="btn btn-default" type="submit">
+						        <i class="glyphicon glyphicon-search"></i>
+						      </button>
+						    </div>
+						  </div>
+						</form>
+					</div>
+		</nav>
 	</div>
 </div>
 </div>
